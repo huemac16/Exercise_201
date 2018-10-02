83f885019b39f497f2c83989947eb8dd8e0cb184
huemac16 <huemac16@htl-kaindorf.at>
2018-10-02 18:55:39 +0200

    created ArrayList

--- Exercise_201/src/VelocityTableModel.java
+++ Exercise_201/src/VelocityTableModel.java
@@ -1,16 +1,19 @@
 
+import java.util.ArrayList;
 import javax.swing.table.AbstractTableModel;
 
 public class VelocityTableModel extends AbstractTableModel {
 
+    private ArrayList<Measurement> list = new ArrayList<>();
+
     @Override
     public int getRowCount() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return list.size();
     }
 
     @Override
     public int getColumnCount() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return 6;
     }
 
     @Override
