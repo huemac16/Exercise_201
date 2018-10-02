ce3e539c1213522cf3ba531547c5531cda12e653
huemac16 <huemac16@htl-kaindorf.at>
2018-10-02 19:03:10 +0200

    added the add method

--- Exercise_201/src/VelocityTableModel.java
+++ Exercise_201/src/VelocityTableModel.java
@@ -6,6 +6,12 @@ public class VelocityTableModel extends AbstractTableModel {
 
     private ArrayList<Measurement> list = new ArrayList<>();
 
+    public void add(Measurement m) {
+        list.add(m);
+
+        fireTableRowsInserted(list.size() - 1, list.size() - 1);
+    }
+
     @Override
     public int getRowCount() {
         return list.size();
@@ -18,7 +24,8 @@ public class VelocityTableModel extends AbstractTableModel {
 
     @Override
     public Object getValueAt(int i, int i1) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Measurement s = list.get(i);
+        return s;
     }
 
 }
