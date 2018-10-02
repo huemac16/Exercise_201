61f19f7ef12cea3ddcad0d53214804f3abc79125
huemac16 <huemac16@htl-kaindorf.at>
2018-10-02 19:19:08 +0200

    finished class

--- Exercise_201/src/VelocityTableRenderer.java
+++ Exercise_201/src/VelocityTableRenderer.java
@@ -1,13 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author marce
- */
-public class VelocityTableRenderer {
-    
+import java.awt.Color;
+import java.awt.Component;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
+
+public class VelocityTableRenderer implements TableCellRenderer {
+
+    @Override
+    public Component getTableCellRendererComponent(JTable jtable, Object o, boolean bln, boolean bln1, int i, int i1) {
+        Measurement m = (Measurement) o;
+        JLabel label = new JLabel();
+        label.setOpaque(true);
+        label.setBackground(Color.WHITE);
+        label.setForeground(Color.BLACK);
+
+        switch (i1) {
+            case 0:
+                label.setText(m.getDate().toString());
+                break;
+            case 1:
+                label.setText(m.getTime().toString());
+                break;
+            case 2:
+                label.setText(m.getLicense());
+                break;
+            case 3:
+                label.setText("" + m.getVel() + " km/h");
+                break;
+            case 4:
+                label.setText("" + m.getAllowed() + " km/h");
+                break;
+            case 5:
+                label.setText("" + m.getVelocityDifference());
+
+                if (m.getVelocityDifference() > 30) {
+                    label.setBackground(Color.RED);
+                } else if (m.getVelocityDifference() < 30 && m.getVelocityDifference() >= 20) {
+                    label.setBackground(Color.ORANGE);
+                } else if (m.getVelocityDifference() < 20 && m.getVelocityDifference() >= 10) {
+                    label.setBackground(Color.YELLOW);
+                } else if (m.getVelocityDifference() < 10) {
+                    label.setBackground(Color.BLUE);
+                }
+                break;
+        }
+
+        return label;
+    }
+
 }
